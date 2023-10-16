e2faac74c4804b1ef7999564c2ddaf9af20be67e
lucianosch <luciano.schiavone@ittgiorgi.edu.it>
2023-10-16 15:00:04 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@MinMaxTreNumeri/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int a,b,c;
+  int min,max;
+  printf("Inserisci tre numeri interi: ");
+  scanf("%d %d %d",&a, &b, &c);
+  if ((a<b && a<c) || (a>b && a>c){
+    if (a<b && b>c){
+      min = a;
+      max = b;
+    } else {
+      min
+    }
+  }
+  return 0;
+}
